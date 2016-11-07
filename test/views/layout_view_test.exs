01a4f4cf02d6f18620760ce0bb35eeabd4f8befd
zee-german <franz.lutzenberger@gmail.com>
2016-11-07 16:23:11 +0100

    tests for displaying session management

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,31 @@
 defmodule BlogPhoenix.LayoutViewTest do
   use BlogPhoenix.ConnCase, async: true
+
+  alias BlogPhoenix.LayoutView
+  alias BlogPhoenix.User
+
+  setup do
+    User.changeset(%User{}, %{username: "test", password: "test", password_confirmation: "test", email: "test@test.com"})
+    |> Repo.insert
+    {:ok, conn: build_conn()}
+  end
+
+  test "current user returns the user in the session", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), user: %{username: "test", password: "test"}
+    assert LayoutView.current_user(conn)
+  end
+
+  test "current user returns nothing if there is no user in the session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute LayoutView.current_user(conn)
+  end
+
+  test "deletes the user session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Signed out successfully!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
 end
